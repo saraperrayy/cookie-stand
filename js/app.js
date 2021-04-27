b41fa4fc86e1f83206e79f1705d59904cfaccbe4
SARA PERRY <SARAPERRYY@GMAIL.COM>
2021-04-27 15:36:01 -0500

    set up js file, added index (home) and sales html files

---
+++ js/app.js
@@ -0,0 +1,104 @@
+// Stores the min/max hourly customers, and the average cookies per customer, in object properties
+
+// Uses a method of that object to generate a random number of customers per hour. Objects/Math/random
+
+// Calculate and store the simulated amounts of cookies purchased for each hour at each location using average cookies purchased and the random number of customers generated
+
+// Store the results for each location in a separate array… perhaps as a property of the object representing that location
+
+// Display the values of each array as unordered lists in the browser
+
+// object literal = locations
+
+const hours = ['6 a.m.', '7 a.m.', '8 a.m.', '9 a.m.', '10 a.m.', '11 a.m.', '12 p.m.', '1 p.m.', '2 p.m.', '3 p.m.', '4 p.m.', '5 p.m.', '6 p.m.', '7 p.m.']
+
+const Seattle = {
+    minSale: 23,
+    maxSale: 64,
+    cookiePerSale: 6.3,
+    cookiePerHr = [] //value in array
+    customerPerHr = [] //value in array
+    totalDailyCookie: //number
+    calcCookiePerHr: function() {
+        
+    }
+    calcCustomerPerHr: function() {
+         
+    }
+}
+render() {
+
+}
+
+const Tokyo = {
+    minCustomers: 3
+    maxCustomers: 24
+    cookiePerSale: 1.2
+    cookiePerHr = [] //value in array
+    customerPerHr = [] //value in array
+    totalDailyCookie: //number
+    calcCookiePerHr: function() {
+        
+    }
+    calcCustomerPerHr: function() {
+         
+    }
+}
+render() {
+
+}
+  
+
+const Dubai = {
+    minCustomers: 11
+    maxCustomers: 65
+    cookiePerSale: 3.7
+    cookiePerHr = [] //value in array
+    customerPerHr = [] //value in array
+    totalDailyCookie: //number
+    calcCookiePerHr: function() {
+        
+    }
+    calcCustomerPerHr: function() {
+         
+    }
+}
+render() {
+
+}
+
+const Paris = {
+    minCustomers: 20
+    maxCustomers: 38
+    cookiePerSale: 2.3
+    cookiePerHr = [] //value in array
+    customerPerHr = [] //value in array
+    totalDailyCookie: //number
+    calcCookiePerHr: function() {
+        
+    }
+    calcCustomerPerHr: function() {
+         
+    }
+}
+render() {
+
+}
+
+const Lima = {
+    minCustomers: 2
+    maxCustomers: 16
+    cookiePerSale: 4.6
+    cookiePerHr = [] //value in array
+    customerPerHr = [] //value in array
+    totalDailyCookie: //number
+    calcCookiePerHr: function() {
+        
+    }
+    calcCustomerPerHr: function() {
+         
+    }
+}
+render() {
+
+}
